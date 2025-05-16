cfd738fa971b6a8d5e9230e03ff770052491af06
mohamedkeshawy <mokeshawy@gmail.com>
2025-05-16 20:42:33 +0300

    update build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.irosinfo"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "1.4.0"
+        versionCode = 5
+        versionName = "1.5.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
