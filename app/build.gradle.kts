6dbb258646faa580803ef1c6154210653223c929
mohamed <mohamed.kishawy@holouly.co>
2025-08-31 06:43:03 +0300

    update build number

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.irosinfo"
         minSdk = 24
         targetSdk = 36
-        versionCode = 6
-        versionName = "1.5.2"
+        versionCode = 7
+        versionName = "1.5.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
